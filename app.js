9c7499e69f21635247e67adc2dc9ede18875bb40
Alex <helloalexsr@gmail.com>
2020-11-01 16:52:02 +0100

    done

--- app.js
+++ app.js
@@ -22,4 +22,24 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => {
+  res.render('beers',{beersFromApi});
+})
+  
+  
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(beersFromApi => {
+
+    res.render('random-beer',{beersFromApi});
+  })
+  .catch(error => console.log(error));
+});
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
